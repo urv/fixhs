3ee3b7a5c525ba4c80d07b840a7bbb482c4b9227
Arvin Moezzi <moezzia@gmail.com>
2010-08-07 23:58:42 +0200

    added bodyParser

--- Test/FIXParserTest.hs
+++ Test/FIXParserTest.hs
@@ -18,4 +18,9 @@ input2 = pack "35=0|49=BRKR|56=INVMGR|34=235|52=19980604-07:58:28|112=19980604-0
 input3 :: ByteString
 input3 = pack "8=FIX.4.2|9=145|35=D|34=4|49=ABC_DEFG01|52=20090323-15:40:29|56=CCG|115=XYZ|11=NF 0542/03232009|54=1|38=100|55=CVS|40=1|59=0|47=A|60=20090323-15:40:29|21=1|207=N|10=112|"
 
-process = do parse messageParser input3
+
+process = do case parse messageParser input3 of 
+               Done m r -> parseMessageBody r
+               _ -> undefined
+
+parseMessageBody = parse bodyParser 
