37735ebd34813fc8e9e73b1a51a348e8da36432f
Arvin Moezzi <moezzia@gmail.com>
2010-09-18 22:49:36 +0200

    parse fix messages as well

--- Main.hs
+++ Main.hs
@@ -1,5 +1,15 @@
 import Socket.ClientSocket
+import Common.FIXParser
+import Common.FIXMessage
+import Data.Attoparsec hiding ( takeWhile1 )
+import Data.ByteString 
+
 
 main :: IO ()
-main = client (mapM_ print) >> putStr "done"
+-- main = client (mapM_ print) >> putStr "done"
+main = client (mapM_ $ print . parseMessageBody) 
 
+parseMessageBody :: ByteString -> Result [FIXMessage]
+parseMessageBody r = case parse bodyParser r of
+                          Partial f -> f empty
+                          _ -> undefined
