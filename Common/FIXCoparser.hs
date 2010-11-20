2fdeae10495e2b7e99c73ad55d62a09efca3d3f9
Arvin Moezzi <moezzia@gmail.com>
2010-11-20 18:10:02 +0100

    reformatted the file a bit

--- Common/FIXCoparser.hs
+++ Common/FIXCoparser.hs
@@ -13,52 +13,47 @@ import Data.ByteString.Char8 as C
 -- this is just meant for testing 
 -- and needs a lot of cleanup
 --
--- ideas:
--- - maybe use some kind of Monoid, resp. Writer, Builder to put together
---   the ByteString. See also blazer-builder - which was started at ZüriHac :-)
---   --> use Data.Text.Lazy.Builder Monoid 
--- - lazy vs. strict ByteString?
--- - as for parsing we use a Monad, can we use the dual, a Comonad, here?
--- - implement Binary for FIXMessage, then call encode to get the ByteString
+-- ideas and issues:
+-- a) maybe use some kind of Monoid, resp. Writer, Builder to put together
+--    the ByteString. Maybe blazer-builder or Data.Text.Lazy.Builder Monoid
+-- b) lazy vs. strict ByteString?
+-- c) as for parsing we use a Monad, can we use the dual, a Comonad, here?
+-- d) implement Binary for FIXMessage, then call encode to get the ByteString
+--    -> seems to be slow (see comments in blaze-builder). also we just need
+--       one direction, i.e. put - get would be the FIXParser
 
-externalize :: FIXMessage -> ByteString
-externalize (t,v) = tag `append` del `append` val 
-	where
-		val = externalizeFIXValue v
-		tag = C.pack $ show (fromEnum t)
-		del = C.singleton '='
-
-externalizeFIXValue :: FIXValue -> ByteString
-externalizeFIXValue (FIXInt i) = C.pack $ show i 
-externalizeFIXValue (FIXBool b) = C.pack $ show b  
-externalizeFIXValue (FIXString s) = s
-
--- TODO: would this be useful...?
--- instance Binary FIXMessage where
--- 	put (t, v) = put (fromEnum t) >> putWord8 '=' >> put v
--- 	get        = undefined
-
-body :: [FIXMessage] -> ByteString
--- body l = intercalate (C.pack fix_delimiter) (P.map (cons fix_delimiter . externalize) l)
-body l = B.concat $ P.map ((C.cons fix_delimiter) . externalize) l
-
+-- FIX header
 header :: ByteString
 header = C.pack "8=FIX.4.1|"
-
-toString :: FIXTag -> ByteString
-toString = C.pack . show . fromEnum
 
 checksumTag :: ByteString
 checksumTag = toString FIX_CHECKSUM
 
+-- FIX body
+externalize :: FIXMessage -> ByteString
+externalize (t,v) = tag `append` del `append` val 
+	where
+		val = externalize' v
+		tag = C.pack $ show (fromEnum t)
+		del = C.singleton '='
+
+externalize' :: FIXValue -> ByteString
+externalize' (FIXInt i) = C.pack $ show i 
+externalize' (FIXBool b) = C.pack $ show b  
+externalize' (FIXString s) = s
+
+body :: [FIXMessage] -> ByteString
+body l = B.concat $ P.map ((C.cons fix_delimiter) . externalize) l
+
+toString :: FIXTag -> ByteString
+toString = C.pack . show . fromEnum
+
 equals :: ByteString
 equals = C.singleton '='
                  
+-- externalize the FIXMessage
 coparse :: [FIXMessage] -> ByteString
--- coparse l = B.concat $ P.map externalize l
 coparse l = header `append` checksum' `append` body'
 	where 
-		-- FIXME: use ByteString directly
-		-- checksum' = C.pack $ "9=" ++ show (checksum body') 
 		checksum' = checksumTag `append` equals `append` C.pack (show (checksum body'))
 		body' = body l
