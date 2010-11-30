5e5f1e86f4d4372553c8fd5323810786b9d77ca2
Arvin Moezzi <moezzia@gmail.com>
2010-11-30 19:22:35 +0100

    integrated into FIXBidirectionalTest

--- Test/FIXCoparserTest.hs
+++
@@ -1,7 +0,0 @@
-import Common.FIXCoparser
-import Common.FIXMessage
-import Data.ByteString.Char8 as C
-
-input :: [FIXMessage]
-input = [(NA49, FIXString (C.pack "xyz")), (NA49, FIXString (C.pack "abc"))]
-test = coparse input
