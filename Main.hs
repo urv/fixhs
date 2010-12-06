11184972e2c57198f01d79e26d0be0ed2015dca2
Arvin Moezzi <moezzia@gmail.com>
2010-12-06 22:37:23 +0100

    moved out the parsing code from ClientSocket

--- Main.hs
+++ Main.hs
@@ -2,9 +2,29 @@ import Socket.ClientSocket
 import Common.FIXParser
 import Data.Attoparsec 
 import Data.ByteString hiding (putStr)
+import qualified Data.ByteString as B hiding ( putStr )
+import qualified Data.ByteString.Char8 as C
+import Control.Monad.Trans
+import Control.Concurrent
+import Common.FIXParser
 
 main :: IO ()
 -- main = client (mapM_ print) >> putStr "done"
-main = client (mapM_ $ print . parseMessageBody)
-	where parseMessageBody i = case parse bodyParser i of
+main = do c <- newChan
+          cs <- getChanContents c
+          forkIO (process cs)
+	  client (parse' c)
+	  where process = (mapM_ $ print . parseMessageBody)
+	        parseMessageBody i = case parse bodyParser i of
 					Partial f -> f empty
+                                          
+-- TODO: do partial parsing, i.e, combine chunks if they exceed the buffer size 
+-- Use attoparsec-iteratee, see also:
+-- http://okmij.org/ftp/Haskell/Iteratee/IterateeIO-talk-notes.pdf
+parse' :: Chan B.ByteString -> IO B.ByteString -> B.ByteString -> IO B.ByteString
+parse' c refill rest = do result <- parseWith refill messageParser rest
+                          case result of
+                             Done rest r -> writeChan c r >> return rest
+			     Partial f -> fail "partial"
+			     Fail a b c -> fail $ show a ++ show c
+                             -- FIXME: improve error handling
--- Socket/ClientSocket.hs
+++ Socket/ClientSocket.hs
@@ -8,44 +8,24 @@ module Socket.ClientSocket
 -- and any additional buffering, and makes for a beautiful, pure API that doesn't care 
 -- what its input source is."
 
-import Data.Attoparsec
-import qualified Data.ByteString as B hiding ( putStr )
 import Network
 import System.IO
-
-import qualified Data.ByteString.Char8 as C
-import Network.Socket hiding (recv)
+import Data.ByteString hiding ( head )
+import Network.Socket hiding ( recv )
 import Network.Socket.ByteString
-
-import Control.Monad.Trans
-import Common.FIXParser
-
-import Control.Concurrent
 
 server :: String
 server = "127.0.0.1"
 
 port :: Int
 port = 3000
-
--- TODO: do partial parsing, i.e, combine chunks if they exceed the buffer size 
--- Use attoparsec-iteratee, see also:
--- http://okmij.org/ftp/Haskell/Iteratee/IterateeIO-talk-notes.pdf
-parse' :: IO B.ByteString -> Chan B.ByteString -> B.ByteString -> IO B.ByteString
-parse' refill c rest = do result <- parseWith refill messageParser rest
-                          case result of
-                             Done rest r -> writeChan c r >> return rest
-                             _ -> undefined -- FIXME: error handling
                                           
-client :: ([B.ByteString] -> IO ()) -> IO ()
+client :: (IO ByteString -> ByteString -> IO ByteString) -> IO ()
 client process = withSocketsDo $
      do addrinfos <- getAddrInfo Nothing (Just server) (Just $ show port)
         let serveraddr = head addrinfos
         sock <- socket (addrFamily serveraddr) Stream defaultProtocol
         connect sock (addrAddress serveraddr)
-        c <- newChan
-        cs <- getChanContents c
-        forkIO (process cs)
-        consume sock c B.empty
+        consume sock empty
         sClose sock
-          where consume sock c rest = parse' (recv sock 1024) c rest >>= consume sock c
+          where consume sock rest = process (recv sock 1024) rest >>= consume sock
