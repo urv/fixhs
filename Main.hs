9278644e2cb39affb392287bbbd90367b45c54df
Arvin Moezzi <moezzia@gmail.com>
2010-12-06 18:38:13 +0100

    cleanup

--- Main.hs
+++ Main.hs
@@ -4,10 +4,7 @@ import Data.Attoparsec
 import Data.ByteString hiding (putStr)
 
 main :: IO ()
--- TODO: replace print by something useful
 -- main = client (mapM_ print) >> putStr "done"
--- FIXME: parseMessage doesn't work 
--- main = client (mapM_ $ print . parseMessage)
 main = client (mapM_ $ print . parseMessageBody)
 	where parseMessageBody i = case parse bodyParser i of
 					Partial f -> f empty
