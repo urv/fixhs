d25682b9d4252c37747c3d7e8a72a98b943f4545
Arvin Moezzi <moezzia@gmail.com>
2011-08-16 02:01:44 +0200

    cosmetic: removed global expressions for all the tags

--- Common/FIXTag.hs
+++ Common/FIXTag.hs
@@ -7,15 +7,12 @@ import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 import Data.Maybe (fromMaybe)
 
-versionFIXTag   = FIXTag 8  toFIXString
-msgLengthFIXTag = FIXTag 9  toFIXInt
-checkSumFIXTag  = FIXTag 10 toFIXInt
 dummyTag        = FIXTag 12 toFIXString
 
 tagLookupTable :: IntMap FIXTag
-tagLookupTable = LT.insert 9 msgLengthFIXTag $ 
-                 LT.insert 10 checkSumFIXTag $ 
-                 LT.insert 8 versionFIXTag LT.new     
+tagLookupTable = LT.insert 8 (FIXTag 8  toFIXString) $ 
+                 LT.insert 9 (FIXTag 9  toFIXInt) $ 
+                 LT.insert 10 (FIXTag 10 toFIXInt) LT.new     
 
 toFIXTag :: Int -> FIXTag
 toFIXTag i = fromMaybe dummyTag (LT.lookup i tagLookupTable)
