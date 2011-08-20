905c6119a1d591ccb2658b35ca53e78b6a0d7209
Arvin Moezzi <moezzia@gmail.com>
2011-08-20 02:31:10 +0200

    cosmetic: removed FIXTag completely

--- Common/FIXTag.hs
+++
@@ -1,9 +0,0 @@
-module Common.FIXTag where
-
-import Common.FIXMessage ( FIXTag(..) )
-import Common.FIXParserCombinators
-import Common.FIXParser ( toFIXString, toFIXInt, toFIXUTCTimestamp )
-import qualified Data.LookupTable as LT
-import Data.IntMap (IntMap)
-import Data.Maybe (fromMaybe)
-
