84252f5743ba4bcf828c5d3d1ea5f65f86ccd303
Arvin Moezzi <moezzia@gmail.com>
2011-08-22 01:52:20 +0200

    feature: xml: code generator: implemented FIX field generator

--- Utils/Generator.hs
+++ Utils/Generator.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
 import Text.XML.HaXml
 import System.Environment ( getArgs )
+import qualified Data.LookupTable as LT
+import Data.Map ( Map )
+import Data.Maybe ( fromMaybe )
 
+type AttrLookupTable = Map String String
+type ParserLookupTable = Map String String
 
 content :: Element a -> [Content a]
 content (Elem _ _ cs) = cs
@@ -25,11 +31,54 @@ getFieldSpec d = do
         isFields (CElem (Elem n _ _) _ ) = n == "fields"
         isFields _ = False
 
+fromAttributes :: LT.LookupTable String String AttrLookupTable 
+                     => [Attribute] -> AttrLookupTable
+fromAttributes = foldr _insert LT.new 
+    where 
+        _insert :: Attribute -> AttrLookupTable -> AttrLookupTable
+        _insert (k, AttValue ((Left v):_)) = LT.insert k v 
+        _insert (k, _) = LT.insert k ""
+
+toParser :: String -> String
+toParser x = fromMaybe "toString" (LT.lookup x parserLT)
+    where
+        parserLT :: ParserLookupTable
+        parserLT = LT.insert "INT" "toFIXInt" $
+                   LT.insert "STRING" "toFIXString" $
+                   LT.insert "DAYOFMONTH" "toFIXDayOfMonth" $
+                   LT.insert "FLOAT" "toFIXFloat" $
+                   LT.insert "QUANTITY" "toFIXQuantity" $
+                   LT.insert "PRICE" "toFIXPrice" $
+                   LT.insert "PRICEOFFSET" "toFIXPriceOffset" $
+                   LT.insert "AMT" "toFIXAmt" $
+                   LT.insert "BOOL" "toFIXBool" $
+                   LT.insert "MULTIPLEVALUESTRING" "toFIXMultipleValueString" $
+                   LT.insert "CURRENCY" "toFIXCurrency" $
+                   LT.insert "EXCHANGE" "toFIXExchange" $
+                   LT.insert "UTCTIMESTAMP" "toFIXUTCTimestamp" $
+                   LT.insert "UTCTIMEONLY" "toFIXUTCTimeOnly" $
+                   LT.insert "LOCALMKTDATE" "toFIXLocalMktDate" $
+                   LT.new
+
+genField :: Content a -> String
+genField (CElem e _) = 
+    let Just fname = LT.lookup "name" aLookup
+        Just fenum = LT.lookup "number" aLookup            
+        Just fparser = LT.lookup "type" aLookup
+        tparser = toParser fparser
+        in
+        "t" ++ fname ++ " = FIXTag { tnum = " ++ fenum ++ 
+            ", tparser = "  ++ tparser ++ " }\n"
+    where
+        aLookup = fromAttributes $ attributes e
+genField _ = ""
+
 xmlFile :: [String] -> String
 xmlFile xs = xs !! 0
 
 main = do
     args <- getArgs
     xmlContent <- readFile $ xmlFile args
-    let Just fields = getFieldSpec (xmlParse "/dev/null" xmlContent)
-        in print $ length (content fields)
+    let xmlDoc = xmlParse "/dev/null" xmlContent
+        Just fields = getFieldSpec xmlDoc in 
+        print (concat $ map genField (content fields))
