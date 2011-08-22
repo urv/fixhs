ceb25a4673fc13ec4db1c0b16a92374f1fa47919
Arvin Moezzi <moezzia@gmail.com>
2011-08-23 00:40:19 +0200

    feature: utils: code generator: generating header and trailer

--- Utils/Generator.hs
+++ Utils/Generator.hs
@@ -20,23 +20,31 @@ getFIXSpec d = case d of
         -> if n == "fix" then Just es else Nothing
     _   -> Nothing 
 
-getFieldSpec :: Document a -> Maybe (Element a)
-getFieldSpec d = do 
+
+getSpec :: String -> Document a -> Maybe (Element a)
+getSpec name d = do 
     fix <- getFIXSpec d 
-    let fields = filter isFields (content fix) in
-        case fields of 
-            (CElem es _):_ -> return es
+    let specs = filter matchName (content fix) in
+        case specs of 
+            CElem es _ : _ -> return es
             _ -> Nothing
     where
-        isFields (CElem (Elem (N n) _ _) _ ) = n == "fields"
-        isFields _ = False
+        matchName (CElem (Elem (N n) _ _) _) = n == name
+        matchName _ = False
+
+getFieldSpec = getSpec "fields"
+getMessagesSpec = getSpec "messages"
+
+getAttr :: String -> Element a -> Maybe String
+getAttr name e = LT.lookup name $ fromAttributes (attributes e)
+
 
 fromAttributes :: LT.LookupTable String String AttrLookupTable 
                      => [Attribute] -> AttrLookupTable
 fromAttributes = foldr _insert LT.new 
     where 
         _insert :: Attribute -> AttrLookupTable -> AttrLookupTable
-        _insert (N k, AttValue ((Left v):_)) = LT.insert k v 
+        _insert (N k, AttValue (Left v : _)) = LT.insert k v 
         _insert (N k, _) = LT.insert k ""
 
 toParser :: String -> String
@@ -57,7 +65,7 @@ toParser x = fromMaybe "toFIXString" (LT.lookup x parserLT)
                    LT.insert "EXCHANGE" "toFIXExchange" $
                    LT.insert "UTCTIMESTAMP" "toFIXUTCTimestamp" $
                    LT.insert "UTCTIMEONLY" "toFIXUTCTimeOnly" $
-                   LT.insert "LOCALMKTDATE" "toFIXLocalMktDate" $
+                   LT.insert "LOCALMKTDATE" "toFIXLocalMktDate" 
                    LT.new
 
 genField :: Content a -> String
@@ -73,12 +81,36 @@ genField (CElem e _) =
         aLookup = fromAttributes $ attributes e
 genField _ = ""
 
+
+genMessages :: Document a -> String
+genMessages d = undefined
+    where
+        getHeaderSpec = getSpec "header"
+        getTrailerSpec = getSpec "trailer"
+
+        mHeader  = let Just h = getHeaderSpec d in getFields (content h)
+        mTrailer = let Just h = getTrailerSpec d in getFields (content h)
+
+        getNameAttr = getAttr "name"
+        getMsgTypeAttr = getAttr "msgtype"
+
+        getFields :: [Content a] -> [String]
+        getFields [] = []
+        getFields (CElem e@(Elem (N n) _ _) _ : cs) =
+            let rest = getFields cs 
+                eName = fromMaybe undefined (getNameAttr e)
+            in
+                (if n == "field" then (:) eName else id) rest
+        getFields (_ : cs) = getFields cs
+
+
+
 xmlFile :: [String] -> String
-xmlFile xs = xs !! 0
+xmlFile = head 
 
 main = do
     args <- getArgs
     xmlContent <- readFile $ xmlFile args
     let xmlDoc = xmlParse "/dev/null" xmlContent
         Just fields = getFieldSpec xmlDoc in 
-        putStr (concat $ map genField (content fields))
+        putStr (concatMap genField (content fields))
