b94f37ea01c4c849cb7ef2a001b4cdf05ba06a70
Arvin Moezzi <moezzia@gmail.com>
2011-08-31 01:05:44 +0200

    utils: added Criterion benchmark

---
+++ Benchmark.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Criterion.Main
+import Criterion.Config
+import Data.FIX.FIX42
+import Common.FIXParser
+import Common.FIXMessage
+import Data.Attoparsec
+import qualified Data.ByteString.Char8 as C
+import Control.DeepSeq
+
+input = C.pack $ "8=FIX.4.2\SOH9=178\SOH35=8\SOH49=PHLX\SOH56=PERS\SOH52=20071123-05:30:00.000\SOH11=ATOMNOCCC9990900\SOH20=3\SOH150=E\SOH39=E\SOH55=MSFT\SOH167=CS\SOH54=1\SOH38=15\SOH40=2\SOH44=15\SOH58=PHLX EQUITY TESTING\SOH59=0\SOH47=C\SOH32=0\SOH31=0\SOH151=15\SOH14=0\SOH6=0\SOH10=128\SOH"
+
+instance Control.DeepSeq.NFData C.ByteString where
+	rnf = const ()
+
+instance Control.DeepSeq.NFData FIXMessage where
+	rnf = const ()
+
+myConfig = defaultConfig 
+
+main = defaultMainWith myConfig (return ()) 
+       [ bench "Parsing FIX" $ nf (parseOnly (messageP fix42)) input ]
